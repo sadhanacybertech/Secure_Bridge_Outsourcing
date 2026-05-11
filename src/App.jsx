83da161bfe48ad79ee4d82b5a605832496bd7d9f
Ankita Singh <ankitasingh9918@gmail.com>
2026-05-11 18:10:14 +0530

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './Components/Header'
 import './App.css'
 import Footer from './Components/Footer'
+import Home from './Home'
 
 function App() {
 
@@ -9,8 +10,7 @@ function App() {
           <>
                <Header/>
                 
-                <h1 className='text-center'>Welcome to Our App</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolore, velit eum laboriosam, minima pariatur accusamus tempore odit illum unde aliquid recusandae hic odio at enim sed. Ex, fuga dolore?</p>
+               <Home/>
 
                 <Footer/>
 
---
+++ src/Home.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section className="hero-section">
+
+      {/* OVERLAY */}
+      <div className="hero-overlay"></div>
+
+      <div className="container">
+
+        <div className="hero-content">
+
+
+          {/* HEADING */}
+          <h1 className="hero-title">
+            Grow Your Business With
+              Smart Outsourcing Solutions
+            
+          </h1>
+
+          {/* DESCRIPTION */}
+          <p className="hero-description">
+            Secure Bridge Outsourcing helps businesses scale
+            faster with professional outsourcing services,
+            operational support, and growth-driven digital
+            strategies designed for long-term success.
+          </p>
+
+          {/* BUTTONS */}
+          <div className="hero-buttons">
+
+            <button className="hero-btn primary-btn">
+              Get Started
+              <FaArrowRight />
+            </button>
+
+            <button className="hero-btn secondary-btn">
+              Explore Services
+            </button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      {/* BLUR EFFECT */}
+      <div className="hero-blur blur-one"></div>
+      <div className="hero-blur blur-two"></div>
+
+    </section>
+  );
+};
+
+export default Home;
